e5415605738c2f0cd6c72af8932b101948e8a323
Pavel Mikhailov <62296354+e-io@users.noreply.github.com>
2020-04-01 15:47:39 +0300

    Metric time bot is added v.alpha
    This bot shows metric time to users and converts metric time to babylonian and back

---
+++ MetricTimeTgBot/bot.py
@@ -0,0 +1,17 @@
+# @MetricTimeBot
+
+import telebot
+import config
+
+mybot = telebot.TeleBot(config.TOKEN)
+
+@mybot.message_handler(commands=['start'])
+def start(message):
+    mybot.send_message(message.chat.id, 'Welcome to metric time world!')
+
+@mybot.message_handler(content_types=['text'])
+def reply(message):
+    mybot.send_message(message.chat.id, 'Repeat, please')
+
+mybot.polling(none_stop=True)
+
---
+++ MetricTimeTgBot/config.py
@@ -0,0 +1,1 @@
+TOKEN='970024644:AAGRbYpTrpuhplPn5U8lHVhjXI3-iYqYnog'
